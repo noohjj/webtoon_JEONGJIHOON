554a826321a47f0eb67343d448ed3cba3faa73e3
noohjj <yk3216@naver.com>
2024-10-28 04:14:54 +0000

    webtoon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sub02 from "./components/subpages/Sub02";
 import Sub03 from "./components/subpages/Sub03";
 import Sub04 from "./components/subpages/Sub04";
 import Sub05 from "./components/subpages/Sub05";
-
+import PageNotFound from "./components/subpages/PageNotFound";
 
 const Wrap = styled.div`
   max-width: 1400px;
@@ -29,6 +29,7 @@ const App = () => {
             <Route path="/Sub03" element={<Sub03 />} />
             <Route path="/Sub04" element={<Sub04 />} />
             <Route path="/Sub05" element={<Sub05 />} />
+            <Route path="/*" element={<PageNotFound/>}/>
         </Routes>
     </HashRouter>
       
